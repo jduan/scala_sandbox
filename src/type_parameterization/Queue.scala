425a8ee1a5c099019849ef003b2d62ef09df9644
Jingjing Duan <jduan@fitbit.com>
2017-01-22 23:00:22 -0800

    private constructor with a companion object

--- src/type_parameterization/Queue.scala
+++ src/type_parameterization/Queue.scala
@@ -1,9 +1,13 @@
 package type_parameterization
 
-class Queue[T] (
-  private val leading: List[T],
-  private val trailing: List[T]
-) {
+// private makes the primary constructor private to prevent object creation
+class Queue[T] private(
+                        private val leading: List[T],
+                        private val trailing: List[T]
+                      ) {
+  // auxiliary constructor
+  def this(l: List[T]) = this(l, Nil)
+
   private def mirror = {
     if (leading.isEmpty)
       new Queue(trailing.reverse, Nil)
@@ -12,17 +16,26 @@ class Queue[T] (
   }
 
   def head = mirror.leading.head
+
   def tail = {
     val q = mirror
     new Queue(q.leading.tail, q.trailing)
   }
+
   def enqueue(x: T) = new Queue(leading, x :: trailing)
+
   def isEmpty = leading.isEmpty && trailing.isEmpty
+}
+
+// companion object
+object Queue {
+  def apply[T](xs: T*) = new Queue[T](xs.toList, Nil)
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
-    var q = new Queue(List(1, 2, 3), List(4, 5, 6))
+    //    var q = new Queue(List(1, 2, 3, 4, 5, 6))
+    var q = Queue(1, 2, 3, 4, 5, 6)
     while (!q.isEmpty) {
       println(q.head)
       q = q.tail
